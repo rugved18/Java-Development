28195eb0363582fefe0e447c933c71c90f4dc94a
Rugved Dhananjaya Kulkarni <123377912+rugved18@users.noreply.github.com>
2025-01-01 11:19:33 +0530

    Create ToDoController.java

---
+++ TODOLIST/ToDoController.java
@@ -0,0 +1,53 @@
+package ToDoList.demo.Controller;
+
+import ToDoList.demo.Service.NewService;
+import ToDoList.demo.model.ToDoModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todo")
+public class ToDoController {
+
+    @Autowired
+    private NewService newService;
+
+    @GetMapping
+    public List<ToDoModel> GetAllTodo(){
+        return newService.FindAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ToDoModel> GetById(@PathVariable int id){
+        ToDoModel toDoModel = newService.FindToDoByID(id);
+
+        if(toDoModel == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(toDoModel);
+    }
+
+    @PostMapping
+    public ToDoModel addToDo(@RequestBody ToDoModel toDoModel){
+        return newService.AddList(toDoModel);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ToDoModel> update(@PathVariable int id, @RequestBody ToDoModel toDoModel){
+        ToDoModel updateTDo = newService.Update(id, toDoModel);
+
+        if(updateTDo == null){
+            return ResponseEntity.notFound().build();
+        }
+        return  ResponseEntity.ok(updateTDo);
+    }
+
+    @DeleteMapping("/{id}")
+    public  ResponseEntity<Void> delete(@PathVariable int id){
+        newService.DeleteToDo(id);
+        return ResponseEntity.noContent().build();
+    }
+}
