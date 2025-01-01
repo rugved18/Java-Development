edb8c968dc936f9447b22bfc8d7e3c90a23db344
Rugved Dhananjaya Kulkarni <123377912+rugved18@users.noreply.github.com>
2025-01-01 11:25:50 +0530

    Update ToDoService.java

--- TODOLIST/ToDoService.java
+++ TODOLIST/ToDoService.java
@@ -1,48 +1,42 @@
-//Service model
 package ToDoList.demo.Service;
 
 import ToDoList.demo.Repo.ToDoRepo;
 import ToDoList.demo.model.ToDoModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 @Service
-public class ToDoService {
+public class NewService {
 
     @Autowired
     private ToDoRepo toDoRepo;
 
-    public List<ToDoModel> getAllInfo() {
+    public List<ToDoModel> FindAll(){
         return toDoRepo.findAll();
     }
 
-    public  ToDoModel getItById(int id) {
+    public ToDoModel FindToDoByID(int id){
         return toDoRepo.findById(id).orElse(null);
     }
 
     public ToDoModel AddList(ToDoModel toDoModel){
-       return toDoRepo.save(toDoModel);
+       return  toDoRepo.save(toDoModel);
     }
 
-    public ToDoModel UpdateToDo(int id, ToDoModel toDoModel){
-        ToDoModel ToDoExisting = toDoRepo.findById(id).orElse(null);
-        if(ToDoExisting == null){
+    public ToDoModel Update(int id, ToDoModel toDoModel){
+        ToDoModel existingToDo = toDoRepo.findById(id).orElse(null);
+        if(existingToDo == null){
             return null;
         }
-        ToDoExisting.setTitle(toDoModel.getTitle());
-        ToDoExisting.setDescription(toDoModel.getDescription());
-        ToDoExisting.setCompleted(toDoModel.getCompleted());
-
-        return toDoRepo.save(ToDoExisting);
+        existingToDo.setTitle(toDoModel.getTitle());
+        existingToDo.setDescription(toDoModel.getDescription());
+        existingToDo.setCompleted(toDoModel.getCompleted());
+        return toDoRepo.save(existingToDo);
     }
 
-    public  void DeleteById(int id){
-         toDoRepo.deleteById(id);
-
+    public void DeleteToDo(int id){
+        toDoRepo.deleteById(id);
     }
 }
